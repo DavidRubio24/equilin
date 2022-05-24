5de7497eea0dc8a3ba15587c739a5dcd68db4d52
David Rubio <davidrubio.24@gmail.com>
2022-05-24 10:25:33 +0200

    better fps

--- workers.py
+++ workers.py
@@ -5,6 +5,7 @@ import time
 import cv2
 import numpy as np
 from mediapipe.python.solutions.face_mesh import FaceMesh
+from mediapipe.python.solutions.hands import Hands
 
 from utils.queues import RequestQueue, RequestQueueLast, RequestQueueNext
 
@@ -20,7 +21,7 @@ def add_images(images_queue: RequestQueue, video_input=0):
     cap.release()
 
 
-def show_images(images_queue: RequestQueue | RequestQueueLast, delay=10):
+def show_images(images_queue: RequestQueue, delay=10):
     """Show images from the queue."""
     for image, timestamp in images_queue(RequestQueueLast):
         cv2.imshow('Original images.', image)
@@ -30,11 +31,11 @@ def show_images(images_queue: RequestQueue | RequestQueueLast, delay=10):
     cv2.destroyWindow('Original images.')
 
 
-def detect_mace_mesh(images_queue: RequestQueue | RequestQueueLast,
+def detect_face_mesh(images_queue: RequestQueue,
                      mesh_detector: FaceMesh,
                      facemesh_queue: RequestQueue):
-    times = [time.time()]
-    for image, timestamp in images_queue(RequestQueueLast):
+    for image, timestamp, *rest in images_queue(RequestQueueLast):
+        start = time.time()
         results = mesh_detector.process(image).multi_face_landmarks
         if results is None: continue
 
@@ -42,11 +43,29 @@ def detect_mace_mesh(images_queue: RequestQueue | RequestQueueLast,
         landmarks[:, 0] *= image.shape[1]
         landmarks[:, 1] *= image.shape[0]
 
-        facemesh_queue.append((image, timestamp, landmarks))
+        facemesh_queue.append((image, timestamp, landmarks, *rest))
+        end = time.time() - start
+
+        print(f'Face: {1/end} fps', end='\r')
+
+
+def detect_hand_mesh(images_queue: RequestQueue,
+                     mesh_detector: Hands,
+                     handmesh_queue: RequestQueue):
+    times = [time.time()]
+    for image, timestamp, *rest in images_queue(RequestQueueLast):
+        results = mesh_detector.process(image).multi_hand_landmarks
+        if results is None: continue
+
+        landmarks = np.array([(l.x, l.y, l.z) for l in results[0].landmark])
+        landmarks[:, 0] *= image.shape[1]
+        landmarks[:, 1] *= image.shape[0]
+
+        handmesh_queue.append((image, timestamp, landmarks, *rest))
 
         times.append(time.time())
         if len(times) > 60:
-            print(f'{60/(times[-1] - times[-60])} fps', end='\r')
+            print(f'Hand: {60/(times[-1] - times[-60])} fps', end='\r')
 
 
 def show_images_drawn(facemesh_queue: RequestQueueNext):
@@ -65,11 +84,15 @@ def show_images_drawn(facemesh_queue: RequestQueueNext):
 def main(video_input=0):
     images_queue = RequestQueue()
     facemesh_queue = RequestQueue()
+    handmesh_queue = RequestQueue()
     threads = [
         threading.Thread(target=add_images, args=(images_queue, video_input)),
         #  threading.Thread(target=show_images, args=(images_queue,)),
-        threading.Thread(target=detect_mace_mesh, args=(images_queue, FaceMesh(), facemesh_queue)),
-        # threading.Thread(target=show_images_drawn, args=(facemesh_queue,)),
+        threading.Thread(target=detect_face_mesh, args=(images_queue, FaceMesh(), facemesh_queue)),
+        #  threading.Thread(target=detect_hand_mesh, args=(images_queue, Hands(), handmesh_queue)),
+        #  threading.Thread(target=show_images_drawn, args=(facemesh_queue,)),
+        #  threading.Thread(target=show_images_drawn, args=(facemesh_queue,)),
+        #  threading.Thread(target=show_images_drawn, args=(handmesh_queue,)),
     ]
 
     for thread in threads:
