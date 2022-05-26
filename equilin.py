fee7a94d92f57425275f3bcbbc59e44b7623d444
David Rubio <davidrubio.24@gmail.com>
2022-05-26 15:33:13 +0200

    save in file
    Bugfix: threads stop (the workers weren't allowing them).
    
    Extract PPGs and save them in .npy.
    Logger.
    Separate file that starts threads.
    Workers recibe queue and iterate it as is.
    
    TODO: left hand should be prioritized.
    TODO: show plot

---
+++ equilin.py
@@ -0,0 +1,100 @@
+import sys
+import threading
+
+from mediapipe.python.solutions.face_mesh import FaceMesh
+from mediapipe.python.solutions.hands import Hands
+
+from utils.queues import RequestQueue, join_queues, RequestQueueNext, RequestQueueLast
+from workers import add_images, detect_mesh, compute_roi, draw_images, get_ppg, save_array, show_images, roi_hand_points, \
+    roi_hand_combined, save_video
+
+MODE = {int: RequestQueueLast,
+        str: RequestQueueNext}
+
+
+def ppg_face(video_input=0, output_file='ppg.npy'):
+    images_queue   = RequestQueue()
+    facemesh_queue = RequestQueue()
+    roi_face_queue = RequestQueue()
+    drawn_queue    = RequestQueue()
+    ppg_queue      = RequestQueue()
+
+    mode = MODE[type(video_input)]
+
+    # We'll create a thread calling the first item with the second item as arguments.
+    threads = [
+        (add_images,  (video_input,          images_queue)),
+        (detect_mesh, (images_queue(mode),   facemesh_queue, FaceMesh)),
+        (compute_roi, (facemesh_queue(mode), roi_face_queue)),
+        (draw_images, (roi_face_queue,       drawn_queue)),
+        (show_images, (drawn_queue,   'Face')),
+        (get_ppg,     (roi_face_queue(mode), ppg_queue)),
+        (save_array, (ppg_queue(mode),       output_file)),
+    ]
+
+    for thread in threads:
+        threading.Thread(target=thread[0], args=thread[1], kwargs=thread[2] if len(thread) > 2 else {}).start()
+    return images_queue
+
+
+def ppg_hand(video_input=0, output_face_file='ppg_face.npy', output_hand_file='ppg_hand.npy'):
+    images_queue   = RequestQueue()
+    facemesh_queue = RequestQueue()
+    handmesh_queue = RequestQueue()
+    roi_face_queue = RequestQueue()
+    roi_hand_queue = RequestQueue()
+    drawn_queue    = RequestQueue()
+    ppg_face_queue = RequestQueue()
+    ppg_hand_queue = RequestQueue()
+
+    mode = MODE[type(video_input)]
+
+    # We'll create a thread calling the first item with the second item as arguments and the third as keyword arguments.
+    threads = [
+        (add_images,  (video_input,    images_queue)),
+
+        (detect_mesh, (images_queue(mode),   facemesh_queue, FaceMesh)),
+        (detect_mesh, (images_queue(mode),   handmesh_queue, Hands)),
+
+        (compute_roi, (facemesh_queue(mode), roi_face_queue)),
+        (compute_roi, (handmesh_queue(mode), roi_hand_queue, roi_hand_points, roi_hand_combined)),
+
+        (draw_images, (facemesh_queue, drawn_queue)),
+        (show_images, (drawn_queue,    'Face')),
+
+        (get_ppg,     (roi_face_queue(mode), ppg_face_queue)),
+        (get_ppg,     (roi_hand_queue(mode), ppg_hand_queue)),
+        (save_array,  (ppg_face_queue(mode), output_face_file)),
+        (save_array,  (ppg_hand_queue(mode), output_hand_file)),
+    ]
+
+    for thread in threads:
+        threading.Thread(target=thread[0], args=thread[1], kwargs=thread[2] if len(thread) > 2 else {}).start()
+    return images_queue
+
+
+def record(video_input=0, output_file='video.mp4'):
+    images_queue = RequestQueue()
+
+    threas = [
+        (add_images,  (video_input,    images_queue)),
+        (save_video,  (images_queue,   output_file)),
+        (show_images, (images_queue,   'Video')),
+    ]
+
+    for thread in threas:
+        threading.Thread(target=thread[0], args=thread[1]).start()
+    return images_queue
+
+
+def main(video_input=0, output_files=(), type_='hand'):
+    if type_ == 'face':
+        return ppg_face(video_input, *output_files[:1])
+    elif type_ == 'hand':
+        return ppg_hand(video_input, *output_files[:2])
+    elif type_ == 'record':
+        return record(video_input, *output_files[:1])
+
+
+if __name__ == '__main__':
+    main(*sys.argv[1:])
--- workers.py
+++ workers.py
@@ -1,3 +1,4 @@
+import logging
 import sys
 import threading
 import time
@@ -7,8 +8,14 @@ import numpy as np
 from mediapipe.python.solutions.face_mesh import FaceMesh
 from mediapipe.python.solutions.hands import Hands
 
-from utils.queues import RequestQueue, RequestQueueLast, RequestQueueNext, join_queues_waiting
+from utils.queues import RequestQueue
 from utils.images import isimage
+
+
+log = logging.getLogger(__name__)
+log.addHandler(logging.StreamHandler(sys.stderr))
+log.handlers[-1].setFormatter(logging.Formatter('\x1B[0;34m%(asctime)s - %(name)s.%(funcName)s\t- %(levelname)s - %(message)s\x1B[0m'))
+log.setLevel(logging.DEBUG)
 
 
 def get_results_from_detector(detector: FaceMesh | Hands, image):
@@ -17,7 +24,7 @@ def get_results_from_detector(detector: FaceMesh | Hands, image):
     return processed.__dict__[processed.__doc__[16:36]]
 
 
-def add_images(images_queue: RequestQueue, video_input=0):
+def add_images(video_input, images_queue: RequestQueue):
     """Add images to the queue."""
     cap = cv2.VideoCapture(video_input)
     success = images_queue.more
@@ -26,20 +33,22 @@ def add_images(images_queue: RequestQueue, video_input=0):
         images_queue.append((time.time(), image))
     images_queue.off()
     cap.release()
+    log.debug('End of add_images.')
 
 
 def show_images(images_queue: RequestQueue, title='Images', delay=10):
     """Show images from the queue."""
-    for timestamp, image, *_ in images_queue(RequestQueueLast):
-        cv2.imshow(title, image)
+    for timestamp, image, *_ in images_queue:
+        cv2.imshow(title, image[:, ::-1])
         k = cv2.waitKey(delay)
         if k == 27:  # ESC.
             break
     cv2.destroyWindow(title)
+    log.debug('End of show_images.')
 
 
 def draw_images(images_queue: RequestQueue, drawn_images_queue: RequestQueue, copy=True):
-    for timestamp, *rest in images_queue(RequestQueueLast):
+    for timestamp, *rest in images_queue:
         image = next(filter(isimage, rest))
         image = image.copy() if copy else image
         for landmarks in rest:
@@ -51,79 +60,91 @@ def draw_images(images_queue: RequestQueue, drawn_images_queue: RequestQueue, co
                 x, y = round(x), round(y)
                 cv2.circle(image, (x, y), 1, (255, 255, 255), -1)
         drawn_images_queue.append((timestamp, image, *rest))
+    drawn_images_queue.off()
+    log.debug('End of draw_images.')
 
 
-def detect_mesh(images_queue: RequestQueue, facemesh_queue: RequestQueue, detector_type=FaceMesh):
+def detect_mesh(images_queue: RequestQueue, mesh_queue: RequestQueue | list[RequestQueue],
+                detector_type=FaceMesh):
     """Detect a face in the images."""
     with detector_type() as mesh_detector:
-        for timestamp, image, *rest in images_queue(RequestQueueLast):
+        for timestamp, image, *rest in images_queue:
+            if image is None:
+                log.debug(f'Image {timestamp} is None.')
+                continue
             results = get_results_from_detector(mesh_detector, image)
-            if results is None: continue
+            if results is None:
+                log.debug(f'No detected mesh in {timestamp}.')
+                continue
 
             landmarks = np.array([[(l.x, l.y, l.z) for l in result.landmark] for result in results])
             landmarks[..., 0] *= image.shape[1]
             landmarks[..., 1] *= image.shape[0]
 
-            facemesh_queue.append((timestamp, image, landmarks, *rest))
+            if isinstance(mesh_queue, RequestQueue):
+                mesh_queue.append((timestamp, image, landmarks, *rest))
+            else:
+                # TODO: Left always first.
+                mesh_queue[0].append((timestamp, image, landmarks[0], *rest))
+                if len(landmarks) > 1:
+                    mesh_queue[0].append((timestamp, image, landmarks[1], *rest))
+    if isinstance(mesh_queue, RequestQueue):
+        mesh_queue.off()
+    else:
+        for queue in mesh_queue:
+            queue.off()
+    log.debug('End of detect_mesh.')
 
 
 roi_face_points = (103, 67, 109, 10, 338, 297, 332, 333, 299, 337, 151, 108, 69, 104)
 roi_face_combination = 1.5 * np.eye(len(roi_face_points)) - .5 * np.eye(len(roi_face_points))[:, ::-1]
 
 roi_hand_points = (0, 1, 5, 9, 13, 17)
-roi_hand_combination = 1.0 * np.eye(len(roi_hand_points))
-roi_hand_combination = np.array([[.7,  0., 0., 0., 0., 0.],
-                                 [0.,  .8, 0., 0., 0., 0.],
-                                 [0., .05, 1., 0., 0., 0.],
-                                 [0., .05, 0., 1., 0., 0.],
-                                 [0., .05, 0., 0., 1., 0.],
-                                 [.3, .05, 0., 0., 0., 1.]])
+# roi_hand_combination = 1.0 * np.eye(len(roi_hand_points))
+roi_hand_combined = np.array([[.7,  0., 0., 0., 0., 0.],
+                              [0.,  .8, 0., 0., 0., 0.],
+                              [0., .05, 1., 0., 0., 0.],
+                              [0., .05, 0., 1., 0., 0.],
+                              [0., .05, 0., 0., 1., 0.],
+                              [.3, .05, 0., 0., 0., 1.]])
 
 
 def compute_roi(mesh_queue: RequestQueue, roi_queue: RequestQueue,
                 roi_points=roi_face_points,
                 roi_combination: np.ndarray = roi_face_combination):
-    for timestamp, image, landmarks, *rest in mesh_queue(RequestQueueLast):
+    for timestamp, image, landmarks, *rest in mesh_queue:
         contour = landmarks.reshape(-1, 3)[list(roi_points)]
         contour = np.einsum('ij,ik->kj', contour, roi_combination)
         roi_queue.append((timestamp, image, contour, *rest))
+    roi_queue.off()
+    log.debug('End of compute_roi.')
 
 
-def main(video_input=0):
-    images_queue = RequestQueue()
-
-    facemesh_queue = RequestQueue()
-    handmesh_queue = RequestQueue()
-
-    roi_face_queue = RequestQueue()
-    roi_hand_queue = RequestQueue()
-
-    joined_queue = RequestQueue()
-
-    drawn_face_queue = RequestQueue()
-    drawn_hands_queue = RequestQueue()
-    threads = [
-        threading.Thread(target=add_images, args=(images_queue, video_input)),
-
-        threading.Thread(target=detect_mesh, args=(images_queue, facemesh_queue)),
-        threading.Thread(target=detect_mesh, args=(images_queue, handmesh_queue, Hands)),
-
-        threading.Thread(target=compute_roi, args=(facemesh_queue, roi_face_queue)),
-        threading.Thread(target=compute_roi, args=(handmesh_queue, roi_hand_queue, roi_hand_points, roi_hand_combination)),
-
-        threading.Thread(target=join_queues_waiting, args=(joined_queue, roi_face_queue, roi_hand_queue)),
-
-        #  threading.Thread(target=draw_images, args=(handmesh_queue, drawn_hands_queue)),
-        threading.Thread(target=draw_images, args=(joined_queue, drawn_face_queue)),
-
-        #  threading.Thread(target=show_images, args=(drawn_hands_queue, 'Hands')),
-        threading.Thread(target=show_images, args=(drawn_face_queue, 'Face')),
-    ]
-
-    for thread in threads:
-        thread.start()
-    return images_queue
+def get_ppg(roi_queue: RequestQueue, ppg_queue: RequestQueue):
+    for timestamp, image, contour, *rest in roi_queue:
+        # Get mask from contour.
+        mask = np.zeros(image.shape[:2], dtype=np.uint8)
+        cv2.drawContours(mask, [contour[..., :2].astype(int)], 0, 255, -1)
+        # Get mean of the mask.
+        mean = np.mean(image[mask.astype(bool)], axis=0)
+        ppg_queue.append((timestamp, mean, *rest))
+    ppg_queue.off()
+    log.debug('End of get_ppg.')
 
 
-if __name__ == '__main__':
-    main(*sys.argv[1:])
+def save_array(ppg_queue: RequestQueue, output_file='ppg.npy'):
+    values = []
+    for timestamp, ppg, *rest in ppg_queue:
+        values.append(ppg)
+    np.save(output_file, values)
+    log.debug(f'End of save_ppg (saved in {output_file}).')
+
+
+def save_video(images_queue: RequestQueue, output_file='video.mp4'):
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    out = cv2.VideoWriter(output_file, fourcc, 30.0, (640, 480))
+
+    for timestamp, image in images_queue:
+        out.write(image)
+    out.release()
+    log.debug('End of record.')
