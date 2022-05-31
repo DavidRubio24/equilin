d793d28b5cdd9286392b3a2026e4eca44d9152db
David Rubio <davidrubio.24@gmail.com>
2022-05-31 14:28:52 +0200

    interpolate
    Now there's thread that can linearly interpolate meshes.
    
    New specific functions for cam or video.
    
    Images are RGB except when interacting with openCV.

--- equilin.py
+++ equilin.py
@@ -6,13 +6,14 @@ from mediapipe.python.solutions.hands import Hands
 
 from roi import hand_PoI, hand_comb, forehead_PoI, forehead_comb
 from utils.queues import RequestQueue, RequestQueueNext, RequestQueueLast, join_queues
-from workers import add_images, detect_mesh, compute_roi, draw_images, get_ppg, save_array, show_images, save_video
+from workers import add_images, detect_mesh, compute_roi, draw_images, get_ppg, save_array, show_images, save_video, \
+    interpolate, save_debug
 
 MODE = {int: RequestQueueLast,
         str: RequestQueueNext}
 
 
-def ppg_face(video_input=0, output_file='ppg.npy', duration=10):
+def ppg_face(video_input=0, output_file='ppg.npy', duration=10.):
     images_queue   = RequestQueue()
     facemesh_queue = RequestQueue()
     roi_face_queue = RequestQueue()
@@ -37,7 +38,7 @@ def ppg_face(video_input=0, output_file='ppg.npy', duration=10):
     return images_queue
 
 
-def ppg_hand(video_input=0, output_face_file='ppg_face.npy', output_hand_file='ppg_hand.npy', duration=30):
+def ppg_hand(video_input=0, output_face_file='ppg_face.npy', output_hand_file='ppg_hand.npy', duration=30.):
     images_queue   = RequestQueue()
     facemesh_queue = RequestQueue()
     handmesh_queue = RequestQueue()
@@ -54,8 +55,8 @@ def ppg_hand(video_input=0, output_face_file='ppg_face.npy', output_hand_file='p
     threads = [
         (add_images,  (video_input,    images_queue, duration)),
 
-        (detect_mesh, (images_queue(mode),   facemesh_queue, FaceMesh)),
-        (detect_mesh, (images_queue(mode),   handmesh_queue, Hands)),
+        (detect_mesh, (images_queue(mode),   facemesh_queue, FaceMesh())),
+        (detect_mesh, (images_queue(mode),   handmesh_queue, Hands())),
 
         (compute_roi, (facemesh_queue(mode), roi_face_queue, forehead_PoI, forehead_comb)),
         (compute_roi, (handmesh_queue(mode), roi_hand_queue,     hand_PoI,     hand_comb)),
@@ -75,7 +76,7 @@ def ppg_hand(video_input=0, output_face_file='ppg_face.npy', output_hand_file='p
     return images_queue
 
 
-def ppg_hand_test(video_input=0, output_face_file='ppg_face.npy', output_hand_file='ppg_hand.npy', duration=30):
+def ppg_hand_test(video_input=0, output_face_file='ppg_face.npy', output_hand_file='ppg_hand.npy', duration=30.):
     images_queue   = RequestQueue()
     handmesh_queue = RequestQueue()
     roi_hand_queue = RequestQueue()
@@ -87,7 +88,7 @@ def ppg_hand_test(video_input=0, output_face_file='ppg_face.npy', output_hand_fi
     threads = [
         (add_images,  (video_input,    images_queue, duration)),
 
-        (detect_mesh, (images_queue(mode),   handmesh_queue, Hands)),
+        (detect_mesh, (images_queue(mode),   handmesh_queue, Hands())),
 
         (compute_roi, (handmesh_queue(mode), roi_hand_queue,     hand_PoI,     hand_comb)),
 
@@ -99,7 +100,7 @@ def ppg_hand_test(video_input=0, output_face_file='ppg_face.npy', output_hand_fi
     return images_queue
 
 
-def ppg_face_test(video_input=0, output_file='ppg_face.npy', duration=30):
+def ppg_face_test(video_input=0, output_file='ppg_face.npy', duration=30.):
     images_queue   = RequestQueue()
     facemesh_queue = RequestQueue()
     roi_face_queue = RequestQueue()
@@ -111,7 +112,7 @@ def ppg_face_test(video_input=0, output_file='ppg_face.npy', duration=30):
     threads = [
         (add_images,  (video_input,    images_queue, duration)),
 
-        (detect_mesh, (images_queue(mode),   facemesh_queue)),
+        (detect_mesh, (images_queue(mode),   facemesh_queue, FaceMesh())),
 
         (compute_roi, (facemesh_queue(mode), roi_face_queue, forehead_PoI, forehead_comb)),
 
@@ -123,7 +124,7 @@ def ppg_face_test(video_input=0, output_file='ppg_face.npy', duration=30):
     return images_queue
 
 
-def ppg_face_n_hand_test(video_input=0, output_face_file='ppg_face.npy', output_hand_file='ppg_hand.npy', duration=30):
+def ppg_face_n_hand_test(video_input=0, output_face_file='ppg_face.npy', output_hand_file='ppg_hand.npy', duration=30.):
     images_queue   = RequestQueue()
     facemesh_queue = RequestQueue()
     handmesh_queue = RequestQueue()
@@ -138,8 +139,8 @@ def ppg_face_n_hand_test(video_input=0, output_face_file='ppg_face.npy', output_
     threads = [
         (add_images,  (video_input,    images_queue, duration)),
 
-        (detect_mesh, (images_queue(mode),   facemesh_queue, FaceMesh)),
-        (detect_mesh, (images_queue(mode),   handmesh_queue, Hands)),
+        (detect_mesh, (images_queue(mode),   facemesh_queue, FaceMesh())),
+        (detect_mesh, (images_queue(mode),   handmesh_queue, Hands())),
 
         (compute_roi, (facemesh_queue(mode), roi_face_queue, forehead_PoI, forehead_comb)),
         (compute_roi, (handmesh_queue(mode), roi_hand_queue,     hand_PoI,     hand_comb)),
@@ -154,7 +155,38 @@ def ppg_face_n_hand_test(video_input=0, output_face_file='ppg_face.npy', output_
     return images_queue
 
 
-def record(video_input=0, output_file='video.mp4', duration=10):
+def ppg_interpol_test(video_input=0, output_face_file='ppg_face.npy', output_hand_file='ppg_hand.npy', duration=10.):
+    images_queue   = RequestQueue()
+    handmesh_queue = RequestQueue()
+    interpolated_queue = RequestQueue()
+    drawn_queue    = RequestQueue()
+    roi_hand_queue = RequestQueue()
+    ppg_hand_queue = RequestQueue()
+
+    mode = MODE[type(video_input)]
+
+    # We'll create a thread calling the first item with the second item as arguments and the third as keyword arguments.
+    threads = [
+        (add_images,  (video_input,    images_queue, duration)),
+
+        (detect_mesh, (images_queue(mode),   handmesh_queue, Hands(max_num_hands=1))),
+
+        (interpolate, (images_queue(RequestQueueNext), handmesh_queue(mode), interpolated_queue)),
+
+        (draw_images, (interpolated_queue, drawn_queue)),
+        (show_images, (drawn_queue,   'Hand')),
+
+        (compute_roi, (handmesh_queue(mode), roi_hand_queue, hand_PoI, hand_comb)),
+
+        (get_ppg,     (roi_hand_queue(mode), ppg_hand_queue)),
+    ]
+
+    for thread in threads:
+        threading.Thread(target=thread[0], args=thread[1], kwargs=thread[2] if len(thread) > 2 else {}).start()
+    return images_queue
+
+
+def record(video_input=0, output_file='video.mp4', duration=10.):
     images_queue = RequestQueue()
 
     threas = [
@@ -165,6 +197,189 @@ def record(video_input=0, output_file='video.mp4', duration=10):
 
     for thread in threas:
         threading.Thread(target=thread[0], args=thread[1]).start()
+    return images_queue
+
+
+def face_only_realtime(video_input=0, output_file='ppg_face.npy', duration=10., show=False):
+    images_queue   = RequestQueue()
+    facemesh_queue = RequestQueue()
+    interpolated_queue = RequestQueue()
+    roi_face_queue = RequestQueue()
+    ppg_face_queue = RequestQueue()
+
+    mode = RequestQueueLast
+
+    # We'll create a thread calling the first item with the second item as arguments and the third as keyword arguments.
+    threads = [
+        (add_images,  (video_input,    images_queue, duration)),
+
+        (detect_mesh, (images_queue(RequestQueueLast),   facemesh_queue, FaceMesh())),
+
+        (interpolate, (images_queue(RequestQueueNext), facemesh_queue(RequestQueueNext), interpolated_queue)),
+
+        (compute_roi, (interpolated_queue(RequestQueueNext), roi_face_queue, forehead_PoI, forehead_comb)),
+
+        (get_ppg,     (roi_face_queue(RequestQueueNext), ppg_face_queue)),
+
+        (save_array,  (ppg_face_queue(RequestQueueNext), output_file)),
+    ]
+
+    if show:
+        drawn_queue = RequestQueue()
+        threads.append((draw_images, (interpolated_queue, drawn_queue)))
+        threads.append((show_images, (drawn_queue,        'Face')))
+
+    for thread in threads:
+        threading.Thread(target=thread[0], args=thread[1], kwargs=thread[2] if len(thread) > 2 else {}).start()
+    return images_queue
+
+
+def hand_only_realtime(video_input=0, output_file='ppg_hand.npy', duration=10., show=False):
+    images_queue   = RequestQueue()
+    handmesh_queue = RequestQueue()
+    interpolated_queue = RequestQueue()
+    roi_hand_queue = RequestQueue()
+    ppg_hand_queue = RequestQueue()
+
+    mode = RequestQueueLast
+
+    # We'll create a thread calling the first item with the second item as arguments and the third as keyword arguments.
+    threads = [
+        (add_images,  (video_input,    images_queue, duration)),
+
+        (detect_mesh, (images_queue(RequestQueueLast), handmesh_queue, Hands(max_num_hands=1))),
+
+        (interpolate, (images_queue(RequestQueueNext), handmesh_queue(RequestQueueNext), interpolated_queue)),
+
+        (compute_roi, (interpolated_queue(RequestQueueNext), roi_hand_queue, hand_PoI, hand_comb)),
+
+        (get_ppg,     (roi_hand_queue(RequestQueueNext), ppg_hand_queue)),
+
+        (save_array,  (ppg_hand_queue(RequestQueueNext), output_file)),
+    ]
+
+    if show:
+        drawn_queue = RequestQueue()
+        threads.append((draw_images, (interpolated_queue(RequestQueueLast), drawn_queue)))
+        threads.append((show_images, (drawn_queue(RequestQueueLast),        'Face')))
+
+    for thread in threads:
+        threading.Thread(target=thread[0], args=thread[1], kwargs=thread[2] if len(thread) > 2 else {}).start()
+    return images_queue
+
+
+def face_n_hand_realtime(video_input=0, output_face_file='ppg_face.npy', output_hand_file='ppg_hand.npy', duration=10., show=False):
+    images_queue   = RequestQueue()
+    handmesh_queue = RequestQueue()
+    facemesh_queue = RequestQueue()
+    interpolated_hand_queue = RequestQueue()
+    interpolated_face_queue = RequestQueue()
+    roi_hand_queue = RequestQueue()
+    roi_face_queue = RequestQueue()
+    ppg_hand_queue = RequestQueue()
+    ppg_face_queue = RequestQueue()
+
+    # We'll create a thread calling the first item with the second item as arguments and the third as keyword arguments.
+    threads = [
+        (add_images,  (video_input,    images_queue, duration)),
+
+        (detect_mesh, (images_queue(RequestQueueLast), handmesh_queue, Hands(max_num_hands=1))),
+        (detect_mesh, (images_queue(RequestQueueLast), facemesh_queue, FaceMesh())),
+
+        (interpolate, (images_queue(RequestQueueNext), handmesh_queue, interpolated_hand_queue)),
+        (interpolate, (images_queue(RequestQueueNext), facemesh_queue, interpolated_face_queue)),
+
+        (compute_roi, (interpolated_hand_queue(RequestQueueNext), roi_hand_queue, hand_PoI, hand_comb)),
+        (compute_roi, (interpolated_face_queue(RequestQueueNext), roi_face_queue, forehead_PoI, forehead_comb)),
+
+        (get_ppg,     (roi_hand_queue(RequestQueueNext), ppg_hand_queue)),
+        (get_ppg,     (roi_face_queue(RequestQueueNext), ppg_face_queue)),
+
+        (save_array,  (ppg_hand_queue(RequestQueueNext), output_hand_file)),
+        (save_array,  (ppg_face_queue(RequestQueueNext), output_face_file)),
+    ]
+
+    if show:
+        drawn_queue = RequestQueue()
+        threads.append((draw_images, (interpolated_hand_queue(RequestQueueLast), drawn_queue)))
+        threads.append((show_images, (drawn_queue(RequestQueueLast),        'Face')))
+
+    for thread in threads:
+        threading.Thread(target=thread[0], args=thread[1], kwargs=thread[2] if len(thread) > 2 else {}).start()
+    return images_queue
+
+
+def face_video(video_input=0, output_file='ppg_face.npy', duration=10., show=False):
+    images_queue   = RequestQueue()
+    facemesh_queue = RequestQueue()
+    roi_face_queue = RequestQueue()
+    ppg_face_queue = RequestQueue()
+
+    mode = RequestQueueLast
+
+    # We'll create a thread calling the first item with the second item as arguments and the third as keyword arguments.
+    threads = [
+        (add_images,  (video_input,    images_queue, duration)),
+
+        (detect_mesh, (images_queue(RequestQueueNext),   facemesh_queue, FaceMesh())),
+
+        (compute_roi, (facemesh_queue(RequestQueueNext), roi_face_queue, forehead_PoI, forehead_comb)),
+
+        (get_ppg,     (roi_face_queue(RequestQueueNext), ppg_face_queue)),
+
+        (save_array,  (ppg_face_queue(RequestQueueNext), output_file)),
+    ]
+
+    if show:
+        drawn_queue = RequestQueue()
+        threads.append((draw_images, (facemesh_queue, drawn_queue)))
+        threads.append((show_images, (drawn_queue,        'Face')))
+
+    for thread in threads:
+        threading.Thread(target=thread[0], args=thread[1], kwargs=thread[2] if len(thread) > 2 else {}).start()
+    return images_queue
+
+
+def face_n_hand_video(video_input=0, output_face_file='ppg_face.npy', output_hand_file='ppg_hand.npy', duration=10., show=False):
+    images_queue = RequestQueue()
+    handmesh_queue = RequestQueue()
+    facemesh_queue = RequestQueue()
+    interpolated_hand_queue = RequestQueue()
+    interpolated_face_queue = RequestQueue()
+    roi_hand_queue = RequestQueue()
+    roi_face_queue = RequestQueue()
+    ppg_hand_queue = RequestQueue()
+    ppg_face_queue = RequestQueue()
+
+    mode = RequestQueueLast
+
+    # We'll create a thread calling the first item with the second item as arguments and the third as keyword arguments.
+    threads = [
+        (add_images, (video_input, images_queue, duration)),
+
+        (detect_mesh, (images_queue(RequestQueueNext), handmesh_queue, Hands(max_num_hands=1))),
+        (detect_mesh, (images_queue(RequestQueueNext), facemesh_queue, FaceMesh())),
+
+        (interpolate, (images_queue(RequestQueueNext), handmesh_queue, interpolated_hand_queue)),
+        (interpolate, (images_queue(RequestQueueNext), facemesh_queue, interpolated_face_queue)),
+
+        (compute_roi, (interpolated_hand_queue(RequestQueueNext), roi_hand_queue, hand_PoI, hand_comb)),
+        (compute_roi, (interpolated_face_queue(RequestQueueNext), roi_face_queue, forehead_PoI, forehead_comb)),
+
+        (get_ppg, (roi_hand_queue(RequestQueueNext), ppg_hand_queue)),
+        (get_ppg, (roi_face_queue(RequestQueueNext), ppg_face_queue)),
+
+        (save_array, (ppg_hand_queue(RequestQueueNext), output_hand_file)),
+        (save_array, (ppg_face_queue(RequestQueueNext), output_face_file)),
+    ]
+
+    if show:
+        drawn_queue = RequestQueue()
+        threads.append((draw_images, (facemesh_queue(RequestQueueLast), drawn_queue)))
+        threads.append((show_images, (drawn_queue(RequestQueueLast), 'Face')))
+
+    for thread in threads:
+        threading.Thread(target=thread[0], args=thread[1], kwargs=thread[2] if len(thread) > 2 else {}).start()
     return images_queue
 
 
--- workers.py
+++ workers.py
@@ -18,18 +18,20 @@ log.handlers[-1].setFormatter(logging.Formatter('\x1B[0;34m%(asctime)s - %(name)
 log.setLevel(logging.DEBUG)
 
 
-def add_images(video_input, images_queue: RequestQueue, time_duration=float('inf'), frames_duration=float('inf')):
+def add_images(video_input, images_queue: RequestQueue, duration=None):
     """Add images to the queue from the video_input."""
     cap = cv2.VideoCapture(video_input)
     frame_count = 0
     success, image = cap.read()
     timestamp = time.time()
+    time_duration = duration if isinstance(duration, float) else float('inf')
+    frames_duration = duration if isinstance(duration, int) else float('inf')
     until = timestamp + time_duration
     while (success
            and images_queue.more
            and frame_count < frames_duration
            and timestamp < until):
-        images_queue.append((time.time(), image))
+        images_queue.append((time.time(), image[..., ::-1]))
         success, image = cap.read()
         timestamp = time.time()
         frame_count += 1
@@ -41,7 +43,7 @@ def add_images(video_input, images_queue: RequestQueue, time_duration=float('inf
 def show_images(images_queue: RequestQueue, title='Images', delay=10):
     """Show images from the queue."""
     for timestamp, image, *_ in images_queue:
-        cv2.imshow(title, image[:, ::-1])
+        cv2.imshow(title, image[:, ::-1, ::-1])
         k = cv2.waitKey(delay)
         if k == 27:  # ESC.
             break
@@ -63,20 +65,19 @@ def draw_images(images_queue: RequestQueue, drawn_queue: RequestQueue, copy=True
     log.debug('End of draw_images.')
 
 
-def detect_mesh(images_queue: RequestQueue, mesh_queue: RequestQueue | list[RequestQueue], detector_type):
+def detect_mesh(images_queue: RequestQueue, mesh_queue: RequestQueue | list[RequestQueue], detector):
     """Detect a face in the images."""
-    with detector_type() as mesh_detector:
+    with detector as mesh_detector:
         for timestamp, image, *rest in images_queue:
             if image is None:
                 log.debug(f'Image {timestamp} is None.')
                 continue
-            start = time.time()
             results = get_results_from_detector(mesh_detector, image)
             if results[0] is None:
-                log.debug(f'No detected mesh in {timestamp}.')
+                log.debug(f'No detected {mesh_detector.__class__.__name__} mesh in {timestamp}.')
                 continue
 
-            landmarks = np.array([[(l.x, l.y, l.z) for l in result.landmark] for result in results[0]])
+            landmarks = np.array([[(l.x, l.y, l.z) for l in result.landmark] for result in results[0]])[0]  # TODO:
             landmarks[..., 0] *= image.shape[1]
             landmarks[..., 1] *= image.shape[0]
 
@@ -93,8 +94,6 @@ def detect_mesh(images_queue: RequestQueue, mesh_queue: RequestQueue | list[Requ
                 mesh_queue[0].append((timestamp, image, landmarks[first], *rest))
                 if len(landmarks) > 1:
                     mesh_queue[0].append((timestamp, image, landmarks[second], *rest))
-            end = time.time()
-            print(f'FPS: {1 / (end - start)}  {detector_type.__name__}')
     if isinstance(mesh_queue, RequestQueue):
         mesh_queue.off()
     else:
@@ -128,7 +127,28 @@ def save_array(ppg_queue: RequestQueue, output_file='ppg.npy'):
     values = []
     for timestamp, ppg, *rest in ppg_queue:
         values.append(ppg)
-    np.save(output_file, values)
+
+    output_file_npy = output_file + '.npy' if not output_file.endswith('.npy') else output_file + '.npy'
+    output_file_csv = output_file + '.csv' if not output_file.endswith('.csv') else output_file + '.csv'
+    np.save(output_file_npy, values)
+    np.savetxt(output_file_csv, values, delimiter=',')
+
+    log.debug(f'End of save_ppg (saved in {output_file}).')
+
+
+def save_debug(ppg_queue: RequestQueue, output_file='ppg'):
+    values = []
+    timestamps = []
+    for timestamp, ppg, *rest in ppg_queue:
+        values.append(ppg)
+        timestamps.append(timestamp)
+
+    output_file_npy = output_file + '.npy' if not output_file.endswith('.npy') else output_file + '.npy'
+    output_file_csv = output_file + '.csv' if not output_file.endswith('.csv') else output_file + '.csv'
+    np.save(output_file_npy, values)
+    np.savetxt(output_file_csv, values, delimiter=',')
+    np.savetxt(output_file_csv[:-4] + '.timestamps' + '.csv', timestamps, delimiter=',')
+
     log.debug(f'End of save_ppg (saved in {output_file}).')
 
 
@@ -140,3 +160,74 @@ def save_video(images_queue: RequestQueue, output_file='video.mp4'):
         out.write(image)
     out.release()
     log.debug('End of record.')
+
+
+def interpolate(images_queue: RequestQueue, mesh_queue: RequestQueue, interpolated_queue: RequestQueue):
+    images_iterator = images_queue.__iter__()
+    mesh_iterator = mesh_queue.__iter__()
+    images_since_mesh = []
+    last_landmarks = None
+    last_timestamp = None
+
+    while mesh_iterator.has_next() or mesh_iterator.more:
+        # Get the next landmarks.
+        timestamp_mesh, *rest = next(mesh_iterator, (None,))
+        if timestamp_mesh is None: continue
+        landmarks = next(filter(islandmarks, rest), None)
+        if landmarks is None: continue
+
+        # Get all the images since the last landmarks.
+        while images_iterator.has_next() or images_iterator.more:
+            timestamp_image, image, *rest = next(images_iterator, (None, None))
+            if timestamp_image is None: continue
+            # TODO: deal with images that are ahead of the mesh.
+            images_since_mesh.append((timestamp_image, image))
+            if timestamp_image >= timestamp_mesh:
+                break
+
+        # Interpolate the images.
+        if last_landmarks is None:
+            log.debug(f'No last landmarks. {len(images_since_mesh)} images.')
+            # Worst interpolation ever: use the current landmarks.
+            for timestamp_intermediate_image, image in images_since_mesh:
+                # TODO: should't we copy the landmarks? Probably not.
+                interpolated_queue.append((timestamp_intermediate_image, image, landmarks, *rest))
+        elif last_timestamp is None:
+            # How TF did we end up here? Anyway... interpolate using the frame number. Close enough.
+            log.warning('No last timestamp to interpolate. Using frame number.')
+            total = len(images_since_mesh) - 1
+            for index, (timestamp_intermediate_image, image) in enumerate(images_since_mesh):
+                # Interpolate between the current and the last landmarks.
+                alpha = index / total
+                interpolated_landmarks = (landmarks        * alpha
+                                          + last_landmarks * (1 - alpha))
+                interpolated_queue.append((timestamp_intermediate_image, image, interpolated_landmarks, *rest))
+        else:
+            # log.debug(f'Interpolating between {last_timestamp} and {timestamp_mesh}. {len(images_since_mesh)} images.')
+            total = timestamp_mesh - last_timestamp
+            for index, (timestamp_intermediate_image, image) in enumerate(images_since_mesh):
+                # Interpolate between the current and the last landmarks.
+                alpha = (timestamp_intermediate_image - last_timestamp) / total
+                interpolated_landmarks = (landmarks        * alpha
+                                          + last_landmarks * (1 - alpha))
+                interpolated_queue.append((timestamp_intermediate_image, image, interpolated_landmarks, *rest))
+
+        last_timestamp = timestamp_mesh
+        last_landmarks = landmarks
+        images_since_mesh = []
+
+    # Get all the images after the last landmarks.
+    while images_iterator.has_next() or images_iterator.more:
+        timestamp_image, image, *rest = next(images_iterator, (None, None))
+        if timestamp_image is None: continue
+        # TODO: deal with images that are ahead of the mesh.
+        images_since_mesh.append((timestamp_image, image))
+
+    # The ramaining images don't get to be interpolated, just use the last landmarks.
+    for timestamp_intermediate_image, image in images_since_mesh:
+        log.debug(f'Adding the last images. Timestamp: {timestamp_intermediate_image}, last_landmarks is None {last_landmarks is None}.')
+        interpolated_queue.append((timestamp_intermediate_image, image, last_landmarks))
+
+    interpolated_queue.off()
+
+    log.debug('End of interpolate.')
